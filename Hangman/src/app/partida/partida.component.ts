17d422402735eb317ffe3292faca1c81797923e2
Emeateuve <emeateuve@gmail.com>
2018-02-13 21:58:25 +0100

    Desarrollando partida

--- Hangman/src/app/partida/partida.component.ts
+++ Hangman/src/app/partida/partida.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-partida',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartidaComponent implements OnInit {
 
-  constructor() { }
+  private abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  private palabra = [];
+  private textoInput = '';
+  private existe = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  adivinaLetra(letra){
+    this.existe = false;
+    for(let i = 0; i < this.palabra.length; i++){
+      if(letra == this.palabra[i]){
+        this.existe = true;
+        console.log(letra);
+      }
+    }
+  }
+
+  descomponer(palabra: string) {
+    this.palabra = [];
+    for (let i = 0; i < palabra.length; i++) {
+      this.palabra.push(palabra[i]);
+    }
+  }
+
+  palabraRecibida() {
+    this.descomponer(this.textoInput);
   }
 
 }
